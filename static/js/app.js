05494fa9635c18c1c7781c4e33f68131b0071882
bae elson <baeelson@baes-Air.fios-router.home>
2020-05-16 21:22:35 -0400

    Required portion complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,94 @@
-// from data.js
+/** check out the data keys
+data = [{
+    datetime:           "1/1/2010",
+    city:               "benton",
+    state:              "ar",
+    country:            "us",
+    shape:              "circle",
+    durationMinutes:    "5 mins.",
+    comments:           "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+    }, { }, { }]
+**/
+
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
+// Initial table load
+//▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Input the data
+tableData.forEach(function(sighting) {
+  
+    // Step 1:  Use d3 to append one table row `tr` for each weather report object (no data yet)
+    var row = tbody.append("tr");
+    
+    // Step 2:  Use `Object.entries` to console.log each sighting's details
+    Object.entries(sighting).forEach(function([key, value]) {
+      ////console.log(key, value);
+      
+      // Step 3: Use d3 to append 1 cell per sighting detail (datetime, city, state, etc.)
+      var cell = row.append("td");
+      
+      // Step 4: Use d3 to update each cell's text with said details
+      cell.text(value);
+    });
+});
+
+
+// Date Filter
+//▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    
+    // Log the value to check
+    console.log(inputValue);
+    
+    // Create a filtering function for the date of sighting
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    
+    // Log the value to check
+    console.log(filteredData);
+  
+    // clear the existing data
+    tbody.html("");
+    
+    // Input the data
+    filteredData.forEach(function(sighting) {
+
+        // Step 1:  Use d3 to append one table row `tr` for each weather report object (no data yet)
+        var row = tbody.append("tr");
+        
+        // Step 2:  Use `Object.entries` to console.log each sighting's details
+        Object.entries(sighting).forEach(function([key, value]) {
+        ////console.log(key, value);
+        
+        // Step 3: Use d3 to append 1 cell per sighting detail (datetime, city, state, etc.)
+        var cell = row.append("td");
+        
+        // Step 4: Use d3 to update each cell's text with said details
+        cell.text(value);
+        });
+    });
+};
